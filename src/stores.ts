35de202a12600cfa25a1a31adfc8bac2eb9ff0a3
eesoymilk <qiyoudaoyi@gmail.com>
2023-09-10 20:57:33 +0800

    Add footer

--- src/stores.ts
+++ src/stores.ts
@@ -4,11 +4,3 @@ import type { Book } from './types';
 export const answers = writable<number[]>([]);
 
 export const result = writable<Book | undefined>(undefined);
-
-// count.subscribe((value) => {
-// 	console.log(value);s
-// }); // logs '0'
-
-// count.set(1); // logs '1'
-
-// count.update((n) => n + 1); // logs '2'
